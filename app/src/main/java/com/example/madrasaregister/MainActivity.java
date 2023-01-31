e62ce76db2910abfde0e63b93875ed9799c6104b
Aqib Amin <aqibamin2002@gmail.com>
2023-01-31 19:15:32 +0500

    Git Link Added  and Do Some Fixex

--- app/src/main/java/com/example/madrasaregister/MainActivity.java
+++ app/src/main/java/com/example/madrasaregister/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     Button btn_add_std, btn_show_reocrds, btn_add_record;
     DbHandler dbHandler;
+    ImageView git_link;
     EditText rec_date, rec_sabak, rec_sabki, rec_manzil;
     Spinner stdSpinner;
     ArrayAdapter<Student> spinnerAdapter;
@@ -35,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         rec_sabak = findViewById(R.id.txt_sabak);
         rec_sabki = findViewById(R.id.txt_sabki);
         rec_manzil = findViewById(R.id.txt_manzil);
+        git_link = findViewById(R.id.git_link);
         dbHandler=new DbHandler(this);
         List<Student> students = dbHandler.selectAllStudents();
         spinnerAdapter = new ArrayAdapter<Student>(this, android.R.layout.simple_spinner_item, students);
@@ -114,6 +119,15 @@ public class MainActivity extends AppCompatActivity {
                     dbHandler.insertRecord(record);
                     Toast.makeText(MainActivity.this, "Record Added Successfully!!", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+
+        git_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://github.com/AqibAmin/MadrasaRegister");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
             }
         });
     }
